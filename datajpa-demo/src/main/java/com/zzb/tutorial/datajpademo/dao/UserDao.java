12ef1f809dc783b17abbd1f862d1349098aaad1c
Cary <CaryZhengMy@gmail.com>
2021-09-08 18:27:12 +0800

    Update comment

--- datajpa-demo/src/main/java/com/zzb/tutorial/datajpademo/dao/UserDao.java
+++ datajpa-demo/src/main/java/com/zzb/tutorial/datajpademo/dao/UserDao.java
@@ -20,10 +20,12 @@ public interface UserDao extends JpaRepository<User, Integer> {
     @Query(value = "SELECT * FROM user WHERE user_name = :userName AND password = :password", nativeQuery = true)
     User findByCustomInfo3(@Param("userName") String userName, @Param("password") String password);
 
+    // 注：若不使用jpa提供的内置方法，执行以下方法将不会自动动更新 create_time 和 update_time 等基础字段
     @Modifying
     @Query("UPDATE User u SET u.userName = ?1 WHERE u.id = ?2")
     int updateUserInfo1(@Param("userName") String userName, @Param("id") Integer userId);
 
+    // 注：若不使用jpa提供的内置方法，执行以下方法将不会自动动更新 create_time 和 update_time 等基础字段
     @Modifying
     @Query(value = "UPDATE user SET user.user_name = ?1 WHERE user.id = ?2", nativeQuery = true)
     int updateUserInfo2(String userName, Integer userId);
