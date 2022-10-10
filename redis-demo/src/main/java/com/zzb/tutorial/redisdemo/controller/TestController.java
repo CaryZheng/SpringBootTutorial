e087e493beea46b11a7fe0bc069a45ef47340e81
Cary <CaryZhengMy@gmail.com>
2022-10-10 17:49:12 +0800

    Add redis hash set zset code

--- redis-demo/src/main/java/com/zzb/tutorial/redisdemo/controller/TestController.java
+++ redis-demo/src/main/java/com/zzb/tutorial/redisdemo/controller/TestController.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ZSetOperations;
 import org.springframework.util.DigestUtils;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,6 +12,7 @@ import javax.annotation.Resource;
 import javax.crypto.Mac;
 import javax.crypto.spec.SecretKeySpec;
 import java.util.List;
+import java.util.Set;
 
 @RestController
 @RequestMapping("/test")
@@ -57,5 +59,75 @@ public class TestController {
         List<String> list = stringRedisTemplate.opsForList().range("k_l1", 0, -1);
 
         return list;
+    }
+
+    @GetMapping("hash")
+    public String doHash() {
+        // delete
+        stringRedisTemplate.opsForHash().delete("my_hash", "my_hash_key1");
+
+        // put
+        stringRedisTemplate.opsForHash().put("my_hash", "my_hash_key1", "my_hash_value1");
+        stringRedisTemplate.opsForHash().put("my_hash", "my_hash_key2", "my_hash_value2");
+
+        // get
+        String hashValue = (String) stringRedisTemplate.opsForHash().get("my_hash", "my_hash_key2");
+
+        return hashValue;
+    }
+
+    @GetMapping("set")
+    public String doSet() {
+
+        final String key = "my_set";
+
+        // delete
+        stringRedisTemplate.delete(key);
+
+        // add
+        stringRedisTemplate.opsForSet().add(key, "set_value_1");
+        stringRedisTemplate.opsForSet().add(key, "set_value_2");
+
+        // size
+        Long size = stringRedisTemplate.opsForSet().size(key);
+
+        // remove
+        stringRedisTemplate.opsForSet().remove(key, "set_value_1");
+
+        // is member
+        Boolean isExsited = stringRedisTemplate.opsForSet().isMember(key, "set_value_2");
+
+        // pop
+        String value = stringRedisTemplate.opsForSet().pop(key);
+
+        return value;
+    }
+
+    @GetMapping("sortedSet")
+    public String doSortedSet() {
+        final String key = "my_zset_java";
+
+        // delete
+        stringRedisTemplate.delete(key);
+
+        // add
+        stringRedisTemplate.opsForZSet().add(key, "张三", 98);
+        stringRedisTemplate.opsForZSet().add(key, "李四", 60);
+        stringRedisTemplate.opsForZSet().add(key, "王五", 80);
+        stringRedisTemplate.opsForZSet().add(key, "赵六", 44);
+
+        // get scroe
+        Double score = stringRedisTemplate.opsForZSet().score(key, "李四");
+
+        // get index
+        Long index = stringRedisTemplate.opsForZSet().rank(key, "王五");
+
+        // 按 score 从小到大的顺序进行排序
+        Set<ZSetOperations.TypedTuple<String>> result = stringRedisTemplate.opsForZSet().rangeWithScores(key, 0, -1);
+
+        // 按 score 从大到小的顺序进行排序，取Top3
+        Set<ZSetOperations.TypedTuple<String>> rankTop3 = stringRedisTemplate.opsForZSet().reverseRangeWithScores(key, 0, 2);
+
+        return "ok";
     }
 }
