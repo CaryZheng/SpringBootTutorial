f654f25d481262ee7d19cbd24b1fbf464c11931e
Cary <CaryZhengMy@gmail.com>
2024-11-20 19:59:10 +0800

    Init pdf-demo

---
+++ pdf-demo/src/main/java/com/zzb/tutorial/pdfdemo/PdfDemoApplication.java
@@ -0,0 +1,13 @@
+package com.zzb.tutorial.pdfdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdfDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PdfDemoApplication.class, args);
+    }
+
+}
---
+++ pdf-demo/src/test/java/com/zzb/tutorial/pdfdemo/PdfDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zzb.tutorial.pdfdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdfDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
